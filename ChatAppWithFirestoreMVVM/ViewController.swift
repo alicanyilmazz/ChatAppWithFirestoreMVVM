bc61ed13a7264db021fb1e2ac2b7ff888246a122
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-16 18:27:35 +0300

    Initial Commit

---
+++ ChatAppWithFirestoreMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppWithFirestoreMVVM
+//
+//  Created by alican on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
