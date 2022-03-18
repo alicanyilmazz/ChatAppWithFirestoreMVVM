8455506f76bb34373fbfb71a246df564e1c94ff3
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-18 14:58:15 +0300

    RegistrationController created.

---
+++ ChatAppWithFirestoreMVVM/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  ChatAppWithFirestoreMVVM
+//
+//  Created by alican on 16.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationController : UIViewController{
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
