fc93cb1eca6428e1946fb92b8253dcbe782df2b7
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-21 16:12:59 +0300

    InputContainerView uı constraints changed.(UI Refactored.)

--- ChatAppWithFirestoreMVVM/View/InputContainerView.swift
+++ ChatAppWithFirestoreMVVM/View/InputContainerView.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class InputContainerView : UIView{
+    
+    private var textFormComponent: TextFormComponent?
+        
     init(textField : UITextField ,inputContainerViewConfiguration input: InputContainerViewConfiguration){
         super.init(frame: .zero)
         
@@ -23,14 +26,23 @@ class InputContainerView : UIView{
         imageView.anchor(left: leftAnchor, paddingLeft: 8)
         imageView.setDimensions(height: 20, width: 20)
         
-        addSubview(textField)
-        textField.centerY(inView: self)
-        textField.anchor(left: imageView.rightAnchor , bottom: bottomAnchor , right: rightAnchor , paddingLeft: 8 , paddingBottom: -8)
+        let warningLabel = UILabel()
+        warningLabel.textColor = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
+        warningLabel.font = UIFont.systemFont(ofSize: 9)
+        warningLabel.text = "Email format is not valid."
+        
+        addSubview(warningLabel)
+        warningLabel.anchor(left: leftAnchor,bottom: bottomAnchor,right: rightAnchor,paddingLeft: 8,paddingBottom: 0,paddingRight: 8,height: 11)
         
         let dividerView = UIView()
         dividerView.backgroundColor = input.dividerBackgroundColor
+        
         addSubview(dividerView)
-        dividerView.anchor(left: leftAnchor,bottom: bottomAnchor,right: rightAnchor,paddingLeft: 8,paddingRight: 8,height: 0.75)
+        dividerView.anchor(left: leftAnchor,bottom: warningLabel.topAnchor,right: rightAnchor,paddingLeft: 8,paddingBottom: 4,paddingRight: 8,height: 0.75)
+        
+        addSubview(textField)
+        textField.centerY(inView: self)
+        textField.anchor(left: imageView.rightAnchor , right: rightAnchor , paddingLeft: 8)
         
     }
     
@@ -54,8 +66,12 @@ class InputContainerViewConfiguration{
        dividerBackgroundColor = _dividerBackgroundColor
     }
     
-    static let emailInputContainerViewConfiguration = InputContainerViewConfiguration(_containerHeight: 50, _icon:  #imageLiteral(resourceName: "ic_mail_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1))
+    static let emailInputContainerViewConfiguration = InputContainerViewConfiguration(_containerHeight: 55, _icon:  #imageLiteral(resourceName: "ic_mail_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: .white)
     
-    static let passwordInputContainerViewConfiguration =  InputContainerViewConfiguration(_containerHeight: 50, _icon:  #imageLiteral(resourceName: "ic_lock_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1))
+    static let passwordInputContainerViewConfiguration =  InputContainerViewConfiguration(_containerHeight: 55, _icon:  #imageLiteral(resourceName: "ic_lock_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: .white)
+    
+    static let fullnameInputContainerViewConfiguration = InputContainerViewConfiguration(_containerHeight: 55, _icon:  #imageLiteral(resourceName: "ic_person_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: .white)
+    
+    static let usernameInputContainerViewConfiguration = InputContainerViewConfiguration(_containerHeight: 55, _icon:  #imageLiteral(resourceName: "ic_person_outline_white_2x"), _iconTintColor: UIColor(red: 78/255, green: 80/255, blue: 88/255, alpha: 1), _iconAlpha: 0.88, _dividerBackgroundColor: .white)
 }
 
