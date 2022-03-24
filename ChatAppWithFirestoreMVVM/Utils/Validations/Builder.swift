87ff3271cdd8027960378539403822ab48d27761
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-24 14:54:34 +0300

    ComponentBuilder created.

--- ChatAppWithFirestoreMVVM/Utils/Validations/Builder.swift
+++ ChatAppWithFirestoreMVVM/Utils/Validations/Builder.swift
@@ -6,42 +6,51 @@
 //
 
 import Foundation
+import UIKit
 
-final class formComponentBuilderImpl{
-    private(set) var formContent = [
-        FormSectionComponent(items: [
-            TextFormComponent(sectionType: ComponentSectionType.loginSection.rawValue, componentType: ComponentSectionType.LoginComponentType.email.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Name Entered"))])]),
-            TextFormComponent(sectionType: ComponentSectionType.loginSection.rawValue, componentType: ComponentSectionType.LoginComponentType.password.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Last Name Entered"))])]),
-            ButtonFormItem(sectionType: ComponentSectionType.loginSection.rawValue, componentType: ComponentSectionType.LoginComponentType.email.rawValue)
+final class ComponentBuilder{
+    
+    static let shared = ComponentBuilder()
+    
+    private init(){}
+    
+    private(set) var content = [
+        FormSectionComponent(components: [
+            TextFormComponent(leftIconImage:"ic_mail_outline_white_2x",placeholderName: "Email",placeHolderColor: .white,isSecureTextEntry: false ,sectionType: ComponentSectionType.loginSection.rawValue, componentType: ComponentSectionType.LoginComponentType.email.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.between3to20, error: .custom(message: "Character length must be between 3-20.")),RegexFormItem(pattern: RegexPatterns.email, error: .custom(message: "Invalid email format."))])]),
+            TextFormComponent(leftIconImage:"ic_lock_outline_white_2x",placeholderName: "Password",placeHolderColor: .white,isSecureTextEntry: true ,sectionType: ComponentSectionType.loginSection.rawValue, componentType: ComponentSectionType.LoginComponentType.password.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.standartPassword, error: .custom(message: "At least one uppercase,one number and length 8-15"))])])
         ]),
-        FormSectionComponent(items: [
-            TextFormComponent(sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.email.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Name Entered"))])]),
-            TextFormComponent(sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.password.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Last Name Entered"))])]),
-            TextFormComponent(sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.fullname.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Last Name Entered"))])]),
-            TextFormComponent(sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.username.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.name, error: .custom(message: "Invalid Last Name Entered"))])]),
-            ButtonFormItem(sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.email.rawValue)
+        FormSectionComponent(components: [
+            TextFormComponent(leftIconImage:"ic_mail_outline_white_2x",placeholderName: "Email",placeHolderColor: .white,isSecureTextEntry: false ,sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.email.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.between3to20, error: .custom(message: "Character length must be between 3-20.")),RegexFormItem(pattern: RegexPatterns.email, error: .custom(message: "Invalid email format."))])]),
+            TextFormComponent(leftIconImage:"ic_lock_outline_white_2x",placeholderName: "Password",placeHolderColor: .white,isSecureTextEntry: true ,sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.password.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.standartPassword, error: .custom(message: "At least one uppercase,one number and length 8-15"))])]),
+            TextFormComponent(leftIconImage:"ic_person_outline_white_2x",placeholderName: "Name",placeHolderColor: .white,isSecureTextEntry: false ,sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.fullname.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.between3to15, error: .custom(message: "Character length must be between 3-15.")),RegexFormItem(pattern: RegexPatterns.alphabetic, error: .custom(message: "You can only enter alphabetic characters."))])]),
+            TextFormComponent(leftIconImage:"ic_person_outline_white_2x",placeholderName: "Username",placeHolderColor: .white,isSecureTextEntry: false ,sectionType: ComponentSectionType.registerSection.rawValue, componentType: ComponentSectionType.RegisterComponentType.username.rawValue,validations: [RegexValidationManagerImpl([RegexFormItem(pattern: RegexPatterns.between3to15, error: .custom(message: "Character length must be between 3-15.")),RegexFormItem(pattern: RegexPatterns.alphabetic, error: .custom(message: "You can only enter alphabetic characters."))])])
         ])
     ]
     
-    func update(val: Any , at indexPath: IndexPath){
-        formContent[indexPath.section].items[indexPath].value = val
+    func getData(componentSectionType: Int) -> [String]{
+        let formComponents = content[componentSectionType].components
+        var data : [String] = [String]()
+        for component in formComponents {
+            data.append(component.value as! String)
+        }
+        return data
     }
     
-    func validate(){
+    func update(val: Any , componentSectionType: Int , componentType: Int){
+       content[componentSectionType].components[componentType].value = val
+    }
+    
+    func validate(componentSectionType: Int) -> Bool{
         do {
-            let formComponents = formContent.flatMap{$0.items}.filter{$0.formId != .submit}
+            let formComponents = content[componentSectionType].components
             for component in formComponents {
                 for validator in component.validations{
                     try validator.validate(component.value as Any)
                 }
             }
-            let validValues = formComponents.map{($0.formId.rawValue,$0.value)}
-            let validDict = Dictionary(uniqueKeysWithValues: validValues) as [String : Any]
-            
-            // Passthrough here
-            //user.send(validDict)
+            return true
         } catch  {
-            print("Something is wrong with form \(error)")
+            return false
         }
     }
 }
