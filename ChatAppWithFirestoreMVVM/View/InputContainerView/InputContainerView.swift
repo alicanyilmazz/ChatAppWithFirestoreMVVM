ebe18585d67098c751ea767f432a849c778d58bc
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-24 15:32:56 +0300

    error message refactored.

--- ChatAppWithFirestoreMVVM/View/InputContainerView/InputContainerView.swift
+++ ChatAppWithFirestoreMVVM/View/InputContainerView/InputContainerView.swift
@@ -43,7 +43,7 @@ class InputContainerView : UIView{
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented.")
     }
 }
 
