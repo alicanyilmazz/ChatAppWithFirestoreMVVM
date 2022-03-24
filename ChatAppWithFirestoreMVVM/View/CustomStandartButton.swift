d4615442c48a12c912b75c0228fa110a63665a9f
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-24 19:43:18 +0300

    CustomStandartButton created.

--- ChatAppWithFirestoreMVVM/View/CustomStandartButton.swift
+++ ChatAppWithFirestoreMVVM/View/CustomStandartButton.swift
@@ -7,17 +7,16 @@
 
 import UIKit
 
-/*
-class ButtonContainerView : UIButton{
+class CustomStandartButton : UIButton{
 
-    init(customButtonConfiguration custom : CustomButtonConfiguration){
+    init(standartButtonConfiguration custom : CustomStandartButtonConfiguration){
         super.init(frame: .zero)
         setTitle(custom.title, for: .normal)
         layer.cornerRadius = custom.cornerRadius
         titleLabel?.font = UIFont.boldSystemFont(ofSize: custom.fontSize)
         tintColor = custom.tintColor
         backgroundColor = UIColor(red: 84/255, green: 104/255, blue: 255/255, alpha: 1)
-        
+        setHeight(height: custom.height)
     }
     
     required init?(coder: NSCoder) {
@@ -25,19 +24,21 @@ class ButtonContainerView : UIButton{
     }
 }
 
-class CustomButtonConfiguration{
+class CustomStandartButtonConfiguration{
     let title : String
     let cornerRadius : CGFloat
     let fontSize : CGFloat
     let tintColor : UIColor
     let backgroundColor : UIColor
+    let height : CGFloat
     
-    init(title : String,cornerRadius : CGFloat = 5 ,fontSize : CGFloat = 14,tintColor : UIColor = .white ,backgroundColor : UIColor = UIColor(red: 84/255, green: 104/255, blue: 255/255, alpha: 1)){
+    init(title : String,cornerRadius : CGFloat = 5 ,fontSize : CGFloat = 14,tintColor : UIColor = .white ,backgroundColor : UIColor = UIColor(red: 84/255, green: 104/255, blue: 255/255, alpha: 1),height : CGFloat = 45){
         self.title = title
         self.cornerRadius = cornerRadius
         self.fontSize = fontSize
         self.tintColor = tintColor
         self.backgroundColor = backgroundColor
+        self.height = height
     }
 }
-*/
+
