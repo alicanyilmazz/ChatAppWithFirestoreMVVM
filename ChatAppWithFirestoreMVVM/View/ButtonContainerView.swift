d16cc46859b169ced28ad7edb473a30ad3fd8766
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-24 15:32:24 +0300

    ButtonContainerView created.

---
+++ ChatAppWithFirestoreMVVM/View/ButtonContainerView.swift
@@ -0,0 +1,43 @@
+//
+//  CustomButton.swift
+//  ChatAppWithFirestoreMVVM
+//
+//  Created by alican on 22.03.2022.
+//
+
+import UIKit
+
+/*
+class ButtonContainerView : UIButton{
+
+    init(customButtonConfiguration custom : CustomButtonConfiguration){
+        super.init(frame: .zero)
+        setTitle(custom.title, for: .normal)
+        layer.cornerRadius = custom.cornerRadius
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: custom.fontSize)
+        tintColor = custom.tintColor
+        backgroundColor = UIColor(red: 84/255, green: 104/255, blue: 255/255, alpha: 1)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class CustomButtonConfiguration{
+    let title : String
+    let cornerRadius : CGFloat
+    let fontSize : CGFloat
+    let tintColor : UIColor
+    let backgroundColor : UIColor
+    
+    init(title : String,cornerRadius : CGFloat = 5 ,fontSize : CGFloat = 14,tintColor : UIColor = .white ,backgroundColor : UIColor = UIColor(red: 84/255, green: 104/255, blue: 255/255, alpha: 1)){
+        self.title = title
+        self.cornerRadius = cornerRadius
+        self.fontSize = fontSize
+        self.tintColor = tintColor
+        self.backgroundColor = backgroundColor
+    }
+}
+*/
