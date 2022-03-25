3a96ded5d6d48f32107a941956dc5935f7e7a870
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-25 22:04:51 +0300

    RegisterViewModel refactored.

--- ChatAppWithFirestoreMVVM/Model/RegisterViewModel.swift
+++ ChatAppWithFirestoreMVVM/Model/RegisterViewModel.swift
@@ -8,15 +8,16 @@
 import Foundation
 
 struct RegisterViewModel{
-    var email: String
-    var password : String
-    var username : String
-    var fullName : String
+    var email: String?
+    var password : String?
+    var username : String?
+    var fullName : String?
     
+    /*
     init(email: String, password: String,fullName:String,username: String){
         self.email = email
         self.password = password
         self.fullName = fullName
         self.username = username
-    }
+    }*/
 }
