46d7df5d2e983b38ceb09d134c60ce9c32e54f7c
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-27 14:22:56 +0300

    LoginViewModel refactored.

--- ChatAppWithFirestoreMVVM/Model/LoginViewModel.swift
+++ ChatAppWithFirestoreMVVM/Model/LoginViewModel.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 struct LoginViewModel{
-    var email: String
-    var password : String
+    var email: String?
+    var password : String?
     
+    /*
     init(email: String, password: String){
         self.email = email
         self.password = password
-    }
+    }*/
 }
